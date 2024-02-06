e595c25b14bc9b57dcd80c451fc03970cf220282
Piyush <piyushshinde1902@gmail.com>
2024-02-06 20:31:21 +0530

    adding mapping

--- src/main/java/com/codewithdurgesh/blog/controllers/TempController.java
+++ src/main/java/com/codewithdurgesh/blog/controllers/TempController.java
@@ -1,5 +1,10 @@
 package com.codewithdurgesh.blog.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/temp")
 public class TempController {
 
 }
