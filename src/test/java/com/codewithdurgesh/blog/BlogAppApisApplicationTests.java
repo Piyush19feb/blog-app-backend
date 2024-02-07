4ab330477f0cdeeedf53b06f86f7a55e59d499f0
Piyush Shinde <piyushshinde1902@gmail.com>
2024-02-07 19:27:54 +0530

    user backend api completed

--- src/test/java/com/codewithdurgesh/blog/BlogAppApisApplicationTests.java
+++ src/test/java/com/codewithdurgesh/blog/BlogAppApisApplicationTests.java
@@ -1,13 +1,29 @@
 package com.codewithdurgesh.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codewithdurgesh.blog.repositories.UserRepo;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 
+	@Autowired
+	private UserRepo userRepo;
+	
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void repoTest() {
+		String className = userRepo.getClass().getName();
+		String packageName = userRepo.getClass().getPackageName();
+		
+		System.out.println("Classname: "+className);
+		System.out.println("PackageName: "+packageName);
 	}
 
 }
