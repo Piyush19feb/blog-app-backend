d7b257cccf2b0586694e82d3dcc876e86b626ff5
Piyush Shinde <piyushshinde1902@gmail.com>
2024-02-21 16:07:35 +0530

    Basic authentication completed (Fetched user details from DB)

---
+++ src/main/java/com/codewithdurgesh/blog/entities/Role.java
@@ -0,0 +1,26 @@
+package com.codewithdurgesh.blog.entities;
+
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Data
+public class Role {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+}
